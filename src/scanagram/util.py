1442ff1b6257d539da0d9544b18efaf641a071bc
Jamie Townsend <jamiehntownsend@gmail.com>
2025-07-19 23:35:55 +0200

    Rm unzip2 (unused)

--- src/scanagram/util.py
+++ src/scanagram/util.py
@@ -12,14 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Iterable, TypeVar
-
-T1 = TypeVar("T1")
-T2 = TypeVar("T2")
-T3 = TypeVar("T3")
 
 
-# Copied from jax/_src/util.py because jax.util.safe_map is deprecated
+# Copied from jax/_src/util.py because jax.util.safe_map and safe_zip are
+# deprecated
 def safe_map(f, *args):
     args = list(map(list, args))
     n = len(args[0])
@@ -27,25 +23,12 @@ def safe_map(f, *args):
         assert len(arg) == n, f"length mismatch: {list(map(len, args))}"
     return list(map(f, *args))
 
-def unzip2(xys: Iterable[tuple[T1, T2]]
-           ) -> tuple[tuple[T1, ...], tuple[T2, ...]]:
-    """Unzip sequence of length-2 tuples into two tuples."""
-    # Note: we deliberately don't use zip(*xys) because it is lazily evaluated,
-    # is too permissive about inputs, and does not guarantee a length-2 output.
-    xs: list[T1] = []
-    ys: list[T2] = []
-    for x, y in xys:
-        xs.append(x)
-        ys.append(y)
-    return tuple(xs), tuple(ys)
-
 def safe_zip(*args):
     args = list(map(list, args))
     n = len(args[0])
     for arg in args[1:]:
         assert len(arg) == n, f'length mismatch: {list(map(len, args))}'
     return list(zip(*args))
-
 ###############################################################################
 
 def all_equal(xs):
